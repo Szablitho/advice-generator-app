70bb3905c79b28c3ab4789d59b2449f43ee9467d
Wiktor Szablewski <wiktor.szablewski@outlook.com>
2023-07-11 11:11:51 -1000

    implemented transition when getting new advice, dice rolls on click

--- scripts/advice.js
+++ scripts/advice.js
@@ -1,17 +1,42 @@
-const adviceCard = document.querySelector(".advice");
+const adviceEl = document.querySelector(".advice");
+const adviceCard = document.querySelector(".advice-card");
 const adviceIDel = document.querySelector("#advice-id");
 const diceEl = document.querySelector("#dice-background");
+const dice = document.getElementById("dice");
 
 async function getAdvice() {
+  adviceEl.style.opacity = 0;
+  adviceEl.style.fontSize = "0.5rem";
+  adviceCard.children[0].style.opacity = 0;
   const response = await fetch('https://api.adviceslip.com/advice');
   const advice = await response.json();
 
-  if(!advice) {
+  setTimeout(() => {
+    adviceIDel.textContent = advice.slip.id;
+    adviceEl.textContent = advice.slip.advice;
+    adviceCard.children[0].style.opacity = 1;
+    adviceEl.style.fontSize = 'initial';
+    adviceEl.style.opacity = 1;
+  }, 1000);
+
+  if (!advice) {
     return alert("Something went wrong"); //do alternative message within an advice card
   }
   // update advice card with response from ADVICE SLIP API
-  console.log(advice);
-  adviceIDel.textContent = advice.slip.id;
-  adviceCard.textContent = advice.slip.advice;
+
 }
-diceEl.addEventListener("click",getAdvice);
+
+const sleep = (time) => {
+  return new Promise((resolve) => setTimeout(resolve, time))
+}
+async function rotateDice() {
+  dice.style.transform = "translateY(1.2rem) rotate(-2turn)"; // resetting state
+  setTimeout(() => {
+    dice.style.transform = "translateY(1.2rem) rotate(2turn)"; //animiation
+  }, 2000);
+  
+  
+}
+diceEl.addEventListener("click", getAdvice);
+diceEl.addEventListener("click", rotateDice);
+// diceEl.addEventListener("click",fadeIn);
